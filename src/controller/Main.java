c8f5b3a8cd96b09d903d331f07c2d85a0ba74c72
jwala5 <dubeyshiva98@gmail.com>
2020-04-08 20:48:00 +0530

    FSJ0320A004

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,20 @@
 package controller;
-
+import utility.ConnectionManager;
 import java.sql.Connection;
 
-import utility.ConnectionManager;
+
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
 	// Fill your code
+		Connection c= null;
+		c= ConnectionManager.getconnection();
+		if(c!=null)
+			System.out.println("Connection established");
+		else
+			System.out.println("Check connection");
+		
 		
 	}
 }
